3861691648df48b3f96cc6e2eab37f0cef782607
Kunal Kumar <113764445+champ18ion@users.noreply.github.com>
2023-09-28 20:41:43 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+// App.jsx
+import React, { useState } from 'react';
 import './style.css';
-import ImageGallery from './ImageGallery.jsx'
+import ImageGallery from './ImageGallery.jsx';
+import Header from './Header';
+
 
 export default function App() {
+  const [page, setPage] = useState(1); 
+  const perPage = 50; 
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1); 
+  };
+
+  const LoadMoreButton = ({ onClick }) => {
+    return (
+      <button onClick={onClick} className="load-more-button">
+        Load More
+      </button>
+    );
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <ImageGallery/>
-      <p>Start editing to see some magic happen :)</p>
+      <Header />
+      <ImageGallery page={page} perPage={perPage} />
+      <LoadMoreButton onClick={handleLoadMore} />
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img width="40" height="40" src="https://img.icons8.com/cute-clipart/40/image-gallery.png" alt="image-gallery"/>
+      </div>
+      <div className="site-name">
+        <h1>My Site</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
