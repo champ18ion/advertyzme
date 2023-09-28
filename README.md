0b95720a7c88641036258816e848b6297f42bf87
champ18ion <113764445+champ18ion@users.noreply.github.com>
2023-09-28 10:57:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# advertyzme
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-p9eju2)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+import ImageGallery from './ImageGallery.jsx'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <ImageGallery/>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
